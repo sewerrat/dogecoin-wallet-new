90183090ae7281c534c8ebb8b1854384bd812bb9
langerhans <langerhans@users.noreply.github.com>
2017-02-03 21:39:13 +0100

    Adapt the sweep feature

--- wallet/src/de/schildbach/wallet/ui/send/RequestWalletBalanceTask.java
+++ wallet/src/de/schildbach/wallet/ui/send/RequestWalletBalanceTask.java
@@ -21,12 +21,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -42,17 +46,28 @@ import javax.net.ssl.SSLSocketFactory;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionConfidence;
+import org.bitcoinj.core.TransactionOutput;
 import org.bitcoinj.core.UTXO;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongycastle.util.encoders.Hex;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.JsonDataException;
 import com.squareup.moshi.Moshi;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet_test.R;
@@ -116,67 +131,59 @@ public final class RequestWalletBalanceTask {
         backgroundHandler.post(new Runnable() {
             @Override
             public void run() {
-                org.bitcoinj.core.Context.propagate(Constants.CONTEXT);
+                // Use either dogechain or chain.so
+                List<String> urls = new ArrayList<String>(2);
+                urls.add(Constants.DOGECHAIN_API_URL);
+                // urls.add(Constants.CHAINSO_API_URL); // inactive for now
+                Collections.shuffle(urls, new Random(System.nanoTime()));
 
-                try {
-                    final Map<InetSocketAddress, String> servers = loadElectrumServers(
-                            assets.open(Constants.Files.ELECTRUM_SERVERS_FILENAME));
-                    final Map.Entry<InetSocketAddress, String> server = new LinkedList<>(servers.entrySet())
-                            .get(new Random().nextInt(servers.size()));
-                    final InetSocketAddress socketAddress = server.getKey();
-                    final String type = server.getValue();
-                    log.info("trying to request wallet balance from {}: {}", socketAddress, address);
-                    final Socket socket;
-                    if (type.equalsIgnoreCase("tls")) {
-                        final SocketFactory sf = SSLSocketFactory.getDefault();
-                        socket = sf.createSocket(socketAddress.getHostName(), socketAddress.getPort());
-                        final SSLSession sslSession = ((SSLSocket) socket).getSession();
-                        if (!HttpsURLConnection.getDefaultHostnameVerifier().verify(socketAddress.getHostName(),
-                                sslSession))
-                            throw new SSLHandshakeException("Expected " + socketAddress.getHostName() + ", got "
-                                    + sslSession.getPeerPrincipal());
-                    } else if (type.equalsIgnoreCase("tcp")) {
-                        socket = new Socket();
-                        socket.connect(new InetSocketAddress(socketAddress.getHostName(), socketAddress.getPort()),
-                                5000);
-                    } else {
-                        throw new IllegalStateException("Cannot handle: " + type);
-                    }
-                    final BufferedSink sink = Okio.buffer(Okio.sink(socket));
-                    sink.timeout().timeout(5000, TimeUnit.MILLISECONDS);
-                    final BufferedSource source = Okio.buffer(Okio.source(socket));
-                    source.timeout().timeout(5000, TimeUnit.MILLISECONDS);
-                    final Moshi moshi = new Moshi.Builder().build();
-                    final JsonAdapter<JsonRpcRequest> requestAdapter = moshi.adapter(JsonRpcRequest.class);
-                    final JsonRpcRequest request = new JsonRpcRequest("blockchain.address.listunspent",
-                            new String[] { address.toBase58() });
-                    requestAdapter.toJson(sink, request);
-                    sink.writeUtf8("\n").flush();
-                    final JsonAdapter<JsonRpcResponse> responseAdapter = moshi.adapter(JsonRpcResponse.class);
-                    final JsonRpcResponse response = responseAdapter.fromJson(source);
-                    if (response.id == request.id) {
-                        final Set<UTXO> utxos = new HashSet<>();
-                        for (final JsonRpcResponse.Utxo responseUtxo : response.result) {
-                            final Sha256Hash utxoHash = Sha256Hash.wrap(responseUtxo.tx_hash);
-                            final int utxoIndex = responseUtxo.tx_pos;
-                            final Coin utxoValue = Coin.valueOf(responseUtxo.value);
-                            final Script script = ScriptBuilder.createOutputScript(address);
-                            final UTXO utxo = new UTXO(utxoHash, utxoIndex, utxoValue, responseUtxo.height, false,
-                                    script);
-                            utxos.add(utxo);
+                final StringBuilder url = new StringBuilder(urls.get(0));
+                url.append(address.toString());
+
+                log.debug("trying to request wallet balance from {}", url);
+
+                final Request.Builder request = new Request.Builder();
+                request.url(HttpUrl.parse(url.toString()));
+
+                final Call call = Constants.HTTP_CLIENT.newCall(request.build());
+                try
+                {
+                    final Response response = call.execute();
+                    if (response.isSuccessful()) {
+                        String content = response.body().string();
+                        final JSONObject json = new JSONObject(content);
+                        final int success = json.getInt("success");
+                        if (success != 1) {
+                            throw new IOException("api status " + success + " when fetching unspent outputs");
                         }
 
-                        log.info("fetched {} unspent outputs from {}", response.result.length, socketAddress);
-                        onResult(utxos);
+                        final JSONArray jsonOutputs = json.getJSONArray("unspent_outputs");
+
+                        final Set<UTXO> utxoSet = new HashSet<>();
+                        for (int i = 0; i < jsonOutputs.length(); i++) {
+                            final JSONObject jsonOutput = jsonOutputs.getJSONObject(i);
+
+                            final Sha256Hash utxoHash = Sha256Hash.wrap(jsonOutput.getString("tx_hash"));
+                            final int utxoIndex = jsonOutput.getInt("tx_output_n");
+                            final byte[] utxoScriptBytes = Hex.decode(jsonOutput.getString("script"));
+                            final Coin uxtutx = Coin.valueOf(Long.parseLong(jsonOutput.getString("value")));
+
+                            UTXO utxo = new UTXO(utxoHash, utxoIndex, uxtutx, -1, false, new Script(utxoScriptBytes));
+                            utxoSet.add(utxo);
+                        }
+
+                        log.info("fetched unspent outputs from {}", url);
+                        onResult(utxoSet);
                     } else {
-                        log.info("id mismatch response:{} vs request:{}", response.id, request.id);
-                        onFail(R.string.error_parse, socketAddress.toString());
+                        final String responseMessage = response.message();
+                        log.info("got http error '{}: {}' from {}", response.code(), responseMessage, url);
+                        onFail(R.string.error_http, response.code(), responseMessage);
                     }
-                } catch (final JsonDataException x) {
-                    log.info("problem parsing json", x);
+                } catch (final JSONException x) {
+                    log.info("problem parsing json from " + url, x);
                     onFail(R.string.error_parse, x.getMessage());
                 } catch (final IOException x) {
-                    log.info("problem querying unspent outputs", x);
+                    log.info("problem querying unspent outputs from " + url, x);
                     onFail(R.string.error_io, x.getMessage());
                 }
             }
